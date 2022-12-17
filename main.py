6f2fc8312e5594792c335b53e7e2066a0715b300
lillyschulte <rickschulte@hotmail.nl>
2022-12-17 16:28:01 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import filedialog
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create the main window
+window = tk.Tk()
+window.title("Audio Player")
+
+# Create a label to display the current audio file
+label = tk.Label(window, text="No file selected")
+label.pack()
+
+# Create a button to browse for audio files
+def browse_audio():
+    # Open a file dialog to select an audio file
+    file_path = filedialog.askopenfilename(filetypes=[("Audio files", "*.mp3;*.wav")])
+    # Update the label with the selected file name
+    label.config(text=file_path)
+
+browse_button = tk.Button(window, text="Browse", command=browse_audio)
+browse_button.pack()
+
+# Create a button to play the selected audio file
+def play_audio():
+    # Load the selected audio file
+    pygame.mixer.music.load(label.cget("text"))
+    # Play the audio file
+    pygame.mixer.music.play()
+
+play_button = tk.Button(window, text="Play", command=play_audio)
+play_button.pack()
+
+# Run the main loop
+window.mainloop()
