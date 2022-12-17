14a8e857a0f37f8506592ef03682213423425cf8
lillyschulte <rickschulte@hotmail.nl>
2022-12-17 16:36:14 +0100

    Added Button for added audio

--- main.py
+++ main.py
@@ -13,33 +13,34 @@ window.title("Audio Player")
 label = tk.Label(window, text="No file selected")
 label.pack()
 
-# Create a button to browse for audio files
-def browse_audio():
-    # Open a file dialog to select an audio file
-    file_path = filedialog.askopenfilename(filetypes=[("Audio files", "*.mp3;*.wav")])
-    # Update the label with the selected file name
-    label.config(text=file_path)
-
-browse_button = tk.Button(window, text="Browse", command=browse_audio)
-browse_button.pack()
-
-# Create a button to play the selected audio file
-def play_audio():
-    # Load the selected audio file
-    pygame.mixer.music.load(label.cget("text"))
+def play_audio(file_path):
+    # Load the specified audio file
+    pygame.mixer.music.load(file_path)
     # Play the audio file
     pygame.mixer.music.play()
 
-play_button = tk.Button(window, text="Play", command=play_audio)
-play_button.pack()
+def create_play_button(file_path):
+    # Create a button to play the audio file
+    button = tk.Button(window, text=file_path, command=lambda: play_audio(file_path))
+    button.pack()
 
 def stop_audio():
     # Fade out the audio over 1000 milliseconds (1 second)
     pygame.mixer.music.fadeout(1000)
 
+def browse_audio():
+    # Open a file dialog to select an audio file
+    file_path = filedialog.askopenfilename(filetypes=[("Audio files", "*.mp3;*.wav")])
+    # Update the label with the selected file name
+    label.config(text=file_path)
+    # Create a button to play the audio file
+    create_play_button(file_path)
+
+browse_button = tk.Button(window, text="Browse", command=browse_audio)
+browse_button.pack()
+
 stop_button = tk.Button(window, text="Stop", command=stop_audio)
 stop_button.pack()
-
 
 # Run the main loop
 window.mainloop()
